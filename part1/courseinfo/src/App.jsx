a53d34038aacb50602285ee05efc25cbfc9d57b6
Tahir Dincer <coskun.dincer2014@gmail.com>
2025-02-11 17:16:57 +0300

    'Exercise 1 of part 1'

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,6 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const Header = (props) => {
+  return <h1>{props.course} </h1>;
+};
+const Content = (props) => {
+  return (
+    <div>
+      <p>
+        {props.parts[0]} {props.exercises[0]}
+      </p>
+      <p>
+        {props.parts[1]} {props.exercises[1]}
+      </p>
+      <p>
+        {props.parts[2]} {props.exercises[2]}
+      </p>
+    </div>
+  );
+};
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises
+      {props.exercises[0] + props.exercises[1] + props.exercises[2]}
+    </p>
+  );
+};
 const App = () => {
   const course = "Half Stack application development";
   const part1 = "Fundamentals of React";
@@ -9,20 +35,14 @@ const App = () => {
   const exercises2 = 7;
   const part3 = "State of a component";
   const exercises3 = 14;
+  const parts = [part1, part2, part3];
+  const exercises = [exercises1, exercises2, exercises3];
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3} </p>
+      <Header course={course} />
+      <Content parts={parts} exercises={exercises} />
+      <Total exercises={exercises} />
     </div>
   );
 };
